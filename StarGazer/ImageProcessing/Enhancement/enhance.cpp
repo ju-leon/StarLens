a309f11be945e057dab66e0ddb3cb9494e0a3e11
Leon Jungemeyer <leon.jungemeyer@me.com>
2022-03-18 21:00:13 +0100

    refactor

---
+++ StarGazer/ImageProcessing/Enhancement/enhance.cpp
@@ -0,0 +1,8 @@
+//
+//  enhance.cpp
+//  StarGazer
+//
+//  Created by Leon Jungemeyer on 18.03.22.
+//
+
+#include "enhance.hpp"
---
+++ StarGazer/ImageProcessing/Enhancement/enhance.hpp
@@ -0,0 +1,13 @@
+//
+//  enhance.hpp
+//  StarGazer
+//
+//  Created by Leon Jungemeyer on 18.03.22.
+//
+
+#ifndef enhance_hpp
+#define enhance_hpp
+
+#include <stdio.h>
+
+#endif /* enhance_hpp */
